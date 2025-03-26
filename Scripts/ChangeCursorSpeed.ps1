2fd6be6d6ec350eedc36797b7968c58f6daf9f49
Heikon Costa <heikon.costa@etec.sp.gov.br>
2025-03-26 09:38:13 -0300

    added set cursor speed
    it need logout script

--- Scripts/ChangeCursorSpeed.ps1
+++ Scripts/ChangeCursorSpeed.ps1
@@ -1,20 +1,25 @@
-# in development
-$selection = Read-Host "Select the cursor speed: "
+# working
+$selection = Read-Host "Select the cursor speed (1-20): "
 
 function Set-Defult {
+    Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSensitivity" -Value 5
     Write-Host -f Yellow "Cursor speed set to 5.`n"
 }
 
 function Set-Speed {
-    Write-Host -f Yellow "Cursor speed set to.`n"
+    Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSensitivity" -Value $selection
+    Write-Host -f Yellow "Cursor speed set to $selection.`n"
 }
 
 switch ($selection) {
     "" {
         Set-Defult
     }
+    {$_ -in 1..20} {
+        Set-Speed
+    }
     default {
-        Write-Host -ForegroundColor Red "Invalid selection. Please run the script again and choose 1 or 2."
+        Write-Host -ForegroundColor Red "Invalid selection. Please run the script again and 1-20."
         exit
     }
 }
